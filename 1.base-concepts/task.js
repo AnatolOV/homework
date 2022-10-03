f61ac8de3d881bdf3c353f266a2b9f4643b65d77
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-10-03 18:58:54 +0300

    correct mistakes 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -24,17 +24,64 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-  //let start = "12/11/2023"
-  // не понял как использовать функцию Date, как рассчитать время, на которое берется кредит, поставил просто 12. Подскажите, пожалуйста, как правильно сделать...
-  let time = 12;
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+
+  let intervalYear = new Date(date).getFullYear() - currentYear;
+  console.log(intervalYear);
+  let intervalMonth = intervalYear * 12 - currentMonth + new
+    Date(date).getMonth();
+  console.log(intervalMonth);
+
+  if (percent === String(percent)) {
+    percent = Number(percent);
+    console.log("percent - Строка преобразована в число");
+  } else if (percent === Number(percent)) {
+    percent = percent;
+  } else {
+    console.log(`Параметр <percent> содержит неправильное значение ${percent}`);
+  }
+  if (contribution === String(contribution)) {
+    contribution = Number(contribution);
+    console.log("contribution - Строка преобразована в число");
+  } else if (contribution === Number(contribution)) {
+    contribution = contribution;
+  } else {
+    console.log(`Параметр <contribution> содержит неправильное значение ${contribution}`);
+  }
+  if (amount === String(amount)) {
+    amount = Number(amount);
+    console.log("amount - Строка преобразована в число");
+  } else if (amount === Number(amount)) {
+    amount = amount;
+  } else {
+    console.log(`Параметр <amount> содержит неправильное значение ${amount}`);
+  }
+  if (date === String(date)) {
+    date = Number(date);
+    //console.log(date);
+    console.log("date - Строка преобразована в число");
+  } else if (date === Number(date)) {
+    date = date;
+  } else {
+    console.log(`Параметр <date> содержит неправильное значение ${date}`);
+  }
+
+  let time = intervalMonth;
   let amountBody = amount - contribution;
-  console.log(amountBody);
+  console.log(amountBody + " тело кредита");
   let P = percent / 12 / 100;
   let pamentMonth = amountBody * (P + (P / (((1 + P) ** time) - 1)));
-  console.log(pamentMonth);
+  console.log(Math.ceil((pamentMonth) * 100) / 100);
   totalAmount = Math.ceil((pamentMonth * time) * 100) / 100;
   console.log(totalAmount);
-  // код для задачи №2 писать здесь
+
 
   return totalAmount;
 }
+calculateTotalMortgage(16, 300000, 3000000, "2044, 04");
+
+//percent - % годовых;
+//contribution - первоначальный взнос по кредиту;
+//amount - сумма кредита;
+//date - срок кредита;
